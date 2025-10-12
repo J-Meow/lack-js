0cd11a22cc424dfa34a79af6b77d6f0fdc63d7c3
Jmeow <j@jmeow.net>
2025-10-12 09:20:21 -0500

    Better API requesting

--- index.ts
+++ index.ts
@@ -18,36 +18,46 @@ export class SlackClient {
             this.subdomain = options.subdomain
         }
     }
-    async userBoot() {
-        const response = await (
-            await fetch(
-                `https://${this.subdomain}.slack.com/api/client.userBoot`,
-                {
-                    method: "POST",
-                    headers: {
-                        cookie: `d=${encodeURIComponent(this.xoxd!)}`,
-                        "Content-Type":
-                            "multipart/form-data; boundary=----boundary",
-                    },
-                    body: `------boundary
-Content-Disposition: form-data; name="token"
-
-${this.xoxc!}
-------boundary
-Content-Disposition: form-data; name="version_all_channels"
-
-true
-------boundary--
-`,
-                },
-            )
-        ).json()
-        if (response.ok) {
-            this.userId = response.self.id
-            this.displayName = response.self.profile.display_name
-            this.fullName = response.self.profile.real_name
+    async apiPost(route: string, data: FormData | { [index: string]: any }) {
+        let formData: FormData
+        if (data instanceof FormData) {
+            formData = data
         } else {
-            throw new Error("Slack no likey us")
+            formData = new FormData()
+            Object.keys(data).forEach((key) => {
+                if (key in data) {
+                    formData.set(
+                        key,
+                        data[key] instanceof Blob
+                            ? data[key]
+                            : String(data[key]),
+                    )
+                }
+            })
         }
+        formData.set("token", this.xoxc!)
+        const response = await fetch(
+            `https://${this.subdomain!}.slack.com/api/${route}`,
+            {
+                method: "POST",
+                body: formData,
+                headers: {
+                    cookie: "d=" + encodeURIComponent(this.xoxd!),
+                },
+            },
+        )
+        const json = await response.json()
+        if (!json.ok) {
+            throw new Error("Slack error: " + json.error)
+        }
+        return json
+    }
+    async userBoot() {
+        const response = await this.apiPost("client.userBoot", {
+            version_all_channels: true,
+        })
+        this.userId = response.self.id
+        this.displayName = response.self.profile.display_name
+        this.fullName = response.self.profile.real_name
     }
 }
